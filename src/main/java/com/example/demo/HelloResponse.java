181b70ded96b6f3f77857b75e4014e5db6a737b4
Htet Thura Zaw (Raymond) <htetthurazaw@gmail.com>
2023-05-01 00:45:51 +0800

    first commit

---
+++ src/main/java/com/example/demo/HelloResponse.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HelloResponse {
+
+    private String message;
+}
---
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void givenLoginRequest_AbleToReachApi() throws Exception {
+
+        mockMvc.perform(get("/api/v1/auth/test"))
+                .andExpect(status().isOk());
+    }
+}
