b35627a0127e55eef6d44fea4eb42dd07addf555
Htet Thura Zaw (Raymond) <htetthurazaw@gmail.com>
2023-05-02 00:21:45 +0800

    test

--- src/test/java/com/example/demo/HelloControllerTest.java
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -17,7 +17,7 @@ public class HelloControllerTest {
     @Test
     public void givenLoginRequest_AbleToReachApi() throws Exception {
 
-        mockMvc.perform(get("/api/v1/auth/test")).andExpect(status().isOk());
+        mockMvc.perform(get("/api/v1/auth/test")).andExpect(status().isNotFound());
         assertEquals("hello", "hello");
 
     }
